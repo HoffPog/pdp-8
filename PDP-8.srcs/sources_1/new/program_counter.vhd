d894eff064a686eae6e40b1ff0406889a8291f1a
HoffPog <hoff@PROTEUS>
2025-11-10 18:25:24 +0000

    PC Counter Implementation

--- PDP-8.srcs/sources_1/new/program_counter.vhd
+++ PDP-8.srcs/sources_1/new/program_counter.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,13 +32,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity program_counter is
-    Port ( clock : in STD_LOGIC;
-           pc_out : out STD_LOGIC);
+    Port ( clk : in STD_LOGIC;
+           pc_out : out STD_LOGIC_VECTOR(31 downto 0));
 end program_counter;
 
 architecture Behavioral of program_counter is
-
+    signal count : unsigned(31 downto 0) := (others => '0');
 begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+                count <= count + 1;
+        end if;
+    end process; 
 
+    pc_out <= std_logic_vector(count);
 
 end Behavioral;
